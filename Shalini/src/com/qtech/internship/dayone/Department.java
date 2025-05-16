cc94d3623e3e4ca588776184ea2018561b90d0af
shalini0006 <shaliniramesh6666@gmail.com>
2025-05-16 11:38:07 +0530

    Update Department.java

--- Shalini/src/com/qtech/internship/dayone/Department.java
+++ Shalini/src/com/qtech/internship/dayone/Department.java
@@ -1,5 +1,45 @@
 package com.qtech.internship.dayone;
 
-public class Department {
-
+public class Department extends College {
+private String Deptname;
+private String HodName;
+public String getDeptName() {
+  return DptName;
 }
+  public void setDptName(String dptName) {
+    DptName = dptName;
+}
+public String getHodName() {
+  return HodName;
+}
+@Override  
+public void toString() {
+  return "Dpartment [Department=" + DeptName + "]";
+}
+} 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
