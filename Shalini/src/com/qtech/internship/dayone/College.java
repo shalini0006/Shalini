a6d0f0a505ff69ca83a583ce092c2c7b7d6c0134
shalini0006 <shaliniramesh6666@gmail.com>
2025-05-16 11:25:56 +0530

    Update College.java

--- Shalini/src/com/qtech/internship/dayone/College.java
+++ Shalini/src/com/qtech/internship/dayone/College.java
@@ -1,6 +1,22 @@
-package com.qtech.internship.dayone;
-
-public class College {
-	private 
-
+(package com.qtech.internship.dayone
+	;
+public class College extends Students {
+private Strinng collegeName;
+@Override
+public String toString() {
+   return College [collegeName=" + CollegeName +", CollegeName="
 }
+public void getCollegeName() {
+	return CollegeName;
+}	
+public void setcollegename(String collegeName) {
+    CollegeNamwe = collegeName;
+}	
+publi8c int setCollegeCode() {
+	return CollegeCode;
+}
+private int CollegeCode;
+}	
+	
+	
+	
