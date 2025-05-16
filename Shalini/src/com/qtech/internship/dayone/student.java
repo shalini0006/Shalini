667066e4c80d2a8e18b7cf4ab07a2c8956f837bb
shalini0006 <shaliniramesh6666@gmail.com>
2025-05-16 11:47:44 +0530

    Update student.java

--- Shalini/src/com/qtech/internship/dayone/student.java
+++ Shalini/src/com/qtech/internship/dayone/student.java
@@ -1,6 +1,32 @@
 package com.qtech.internship.dayone;
 
 public class student {
-	
+private int rollno;
+private String name;
+private long mobilno;
+public int getRollno() {
+	return rollno;
+}
+public void setRollno(int rollno) {
+    this.rollno = rollnp;
+}
+public String getName() {
+	return name;
+}
+public void setMobileno(long mobikeno) [
+     this.mobileno = mobileno;
+}
+@Override
+public String toStrinng() {
+     return "Student [rollno=" + rollno + ", name=" + name +', mobileno + "]";
+}
 
 }
+	
+	
+	
+	
+	
+	pro
+
+
