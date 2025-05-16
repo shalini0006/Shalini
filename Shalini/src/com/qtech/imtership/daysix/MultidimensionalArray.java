0a1e6e0190ac3811369de190d0a79bc8e335bc0a
shalini0006 <shaliniramesh6666@gmail.com>
2025-05-16 11:00:32 +0530

    Update MultidimensionalArray.java

--- Shalini/src/com/qtech/imtership/daysix/MultidimensionalArray.java
+++ Shalini/src/com/qtech/imtership/daysix/MultidimensionalArray.java
@@ -2,4 +2,26 @@ package com.qtech.imtership.daysix;
 
 public class MultidimensionalArray {
     public static void main(String[] args) {
-	    Scanner sc = new Scanner
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter number of rows:");
+	    int row = sc.nextInt();
+	    System.out.println("Enter elements of array:");
+	    int col = sc.nextInt();
+	    int[][]arr=new int[row[[col];
+	    System.out.println("Enter elements of array:");
+	    for(int i=0; i < row; i++)
+	    {
+		    for(int j=0; j < col; j++) {
+			arr[i][j]= sc.nextInt();
+		    }
+	    }
+	    System.out.println("Elements of array are:');
+	    for(int i=0; i<row; i++) {
+		for(int j=0; j<col; j++) {
+		   System.out.println(arr[i][j]+" ");
+	    }
+            sc.close();
+
+
+
+	    }		    
